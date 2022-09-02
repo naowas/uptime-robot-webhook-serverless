74dff4a826c78a1d370a62038fd886ed388d33c4
Naowas Morshed Eimon <naowas.morshed.eimon@gmail.com>
2022-09-03 01:20:01 +0600

    human diff time

--- api/webhook.js
+++ api/webhook.js
@@ -4,6 +4,7 @@ process.env.NTBA_FIX_319 = 'test';
 
 // Require our Telegram helper package
 const TelegramBot = require('node-telegram-bot-api');
+const MomentJS = require('moment')
 
 // Export as an asynchronous function
 // We'll wait until we've responded to the user
@@ -16,21 +17,25 @@ module.exports = async (request, response) => {
         const channelID = ["-1001692848614"]
 
         // Retrieve the POST request body that gets sent from Telegram
-        const { body } = request;
+        const {body} = request;
+        let durationHumanDiff = '';
         // Ensure that this is a message being sent
         if (body.title) {
+            if (isNaN(body.duration) === false) {
+                const duration = MomentJS.duration(body.duration);
+                durationHumanDiff ='\n' + '*Duration:* '+duration.humanize();
+            }
             // Send our new message back in Markdown
             // Will implemet ability to send to multiple ids at a time //
-            await bot.sendMessage(channelID[0], body.title + '\n' + body.name + '\n' + body.value + '\n'+ body.url, {parse_mode: 'Markdown'});
+            await bot.sendMessage(channelID[0], body.title + '\n' + body.name + '\n' + body.value + durationHumanDiff + '\n' + body.url, {parse_mode: 'Markdown'});
         }
-    }
-    catch(error) {
-        // If there was an error sending our message then we 
+    } catch (error) {
+        // If there was an error sending our message then we
         // can log it into the Vercel console
         console.error('Error sending message');
         console.log(error.toString());
     }
-    
+
     // Acknowledge the message with Telegram
     // by sending a 200 HTTP status code
     response.send('OK');
